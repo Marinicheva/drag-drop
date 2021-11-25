6b1665184788b9361b6113d2fae0aa318a84c33d
Kate <marri.nich@yandex.ru>
2021-11-25 17:10:44 +0300

    Script for add new task. PS Need to  fix the code for moving task

--- app.js
+++ app.js
@@ -13,6 +13,27 @@ for (const placeholder of placeholders) {
     placeholder.addEventListener('drop', dragDrop);
 }
 
+const taskFormBtn = document.querySelector('.add-task__form-btn');
+const addTaskForm = document.querySelector('.add-task__form-container');
+
+taskFormBtn.addEventListener('click', () => {
+    addTaskForm.classList.toggle('add-task__form_hide');
+});
+
+const addTaskBtn = document.querySelector('.add-task__btn');
+const newTasks = document.querySelector('.placeholder-new');
+var newTask =  document.createElement('div');
+var inputTextTask = document.querySelector('.add-task__text');
+
+
+addTaskBtn.addEventListener('click', (event) => {
+    newTask.classList.add('placeholder__task', 'task');
+    newTask.innerHTML = inputTextTask.value;
+    newTasks.prepend(newTask);
+    addTaskForm.classList.toggle('add-task__form_hide');
+    event.preventDefault();
+});
+
 function dragStart(event) {
     event.target.classList.add('task_hold');
     setTimeout(() => {
